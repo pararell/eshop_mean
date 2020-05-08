4cd381031fa5586862c3f9e0a5750a00817f41c3
Miroslav Smrtic <miroslav.smrtic@pelikan.sk>
2020-05-08 14:03:06 +0200

    feat: prepare order

--- client/src/app/modules/order/orders.component.ts
+++ client/src/app/modules/order/orders.component.ts
@@ -1,11 +1,10 @@
 import { filter } from 'rxjs/operators';
-import { TranslateService } from '../../services/translate.service';
+import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
-
 import { Observable } from 'rxjs';
 
+import { TranslateService } from '../../services/translate.service';
 import * as fromRoot from '../../store/reducers';
-import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-orders',
