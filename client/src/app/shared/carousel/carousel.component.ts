659ee49d15b592904ee4e7fd56b707e21930012c
Miroslav Smrtic <miroslav.smrtic@pelikan.sk>
2020-05-27 11:48:59 +0200

    feat: carousel autoSlide

--- client/src/app/shared/carousel/carousel.component.ts
+++ client/src/app/shared/carousel/carousel.component.ts
@@ -1,48 +1,55 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { BehaviorSubject, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { BehaviorSubject, of, Subscription, timer } from 'rxjs';
+import { delay, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent implements AfterViewInit {
+export class CarouselComponent implements AfterViewInit, OnDestroy {
   @ViewChild('slides') slides: ElementRef<HTMLDivElement>;
   @ViewChild('slideContainer') slideContainer: ElementRef<HTMLDivElement>;
 
   showArrowsSub$ = new BehaviorSubject(false);
+  autoSlideSub: Subscription;
 
   ngAfterViewInit(): void {
     of('slidesChildren')
-      .pipe(delay(300))
+      .pipe(delay(300), take(1))
       .subscribe(() => {
-        if (this.slides.nativeElement.children && this.slides.nativeElement.children[0]) {
+        const slidesElement = this.slides.nativeElement;
+        if (slidesElement.children && slidesElement.children[0]) {
           this.showArrowsSub$.next(
-            this.slides.nativeElement.offsetWidth <
-              this.slides.nativeElement.children[0].clientWidth * this.slides.nativeElement.children.length
+            slidesElement.offsetWidth < slidesElement.children[0].clientWidth * slidesElement.children.length
           );
         } else {
           this.showArrowsSub$.next(true);
         }
       });
+
+    this.autoSlideSub = timer(10000, 8000).subscribe(() => { this.onClickRight(); })
+  }
+
+  ngOnDestroy(): void {
+    if (this.autoSlideSub) {
+      this.autoSlideSub.unsubscribe();
+    }
   }
 
   onClickLeft() {
-    this.slides.nativeElement.scrollLeft -= this.slides.nativeElement.offsetWidth;
-    if (!this.slides.nativeElement.scrollLeft) {
-      this.slides.nativeElement.scrollLeft +=
-        this.slides.nativeElement.offsetWidth * this.slides.nativeElement.children.length - 1;
+    const slidesElement = this.slides.nativeElement;
+    slidesElement.scrollLeft -= slidesElement.offsetWidth;
+    if (!slidesElement.scrollLeft) {
+      slidesElement.scrollLeft += slidesElement.offsetWidth * slidesElement.children.length - 1;
     }
   }
 
   onClickRight() {
-    this.slides.nativeElement.scrollLeft += this.slides.nativeElement.offsetWidth;
-    if (
-      (this.slides.nativeElement.scrollWidth - this.slides.nativeElement.scrollLeft).toFixed() ===
-      this.slides.nativeElement.offsetWidth.toFixed()
-    ) {
-      this.slides.nativeElement.scrollLeft = 0;
+    const slidesElement = this.slides.nativeElement;
+    slidesElement.scrollLeft += slidesElement.offsetWidth;
+    if ((slidesElement.scrollWidth - slidesElement.scrollLeft).toFixed() === slidesElement.offsetWidth.toFixed()) {
+      slidesElement.scrollLeft = 0;
     }
   }
 }
