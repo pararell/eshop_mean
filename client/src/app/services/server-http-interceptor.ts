fe297494f903186ee8840686657178f0ec6f56cf
Miroslav Smrtic <miroslav.smrtic@pelikan.sk>
2020-06-02 17:20:40 +0200

    feat: intercept more API

--- client/src/app/services/server-http-interceptor.ts
+++ client/src/app/services/server-http-interceptor.ts
@@ -8,7 +8,7 @@ import { TransferState, makeStateKey, StateKey } from '@angular/platform-browser
 @Injectable()
 export class ServerHttpInterceptor implements HttpInterceptor {
   key  : StateKey<string>;
-  apiToTransfer = ['api/eshop', 'api/products'];
+  apiToTransfer = ['api/eshop', 'api/products', 'api/translations', 'api/orders'];
 
   constructor(
     @Optional() @Inject('serverUrl') protected serverUrl: string,
@@ -16,7 +16,7 @@ export class ServerHttpInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(tap(event => {
-    if (event instanceof HttpResponse && (request.method === 'GET' 
+    if (event instanceof HttpResponse && (request.method === 'GET'
       && this.apiToTransfer.find(api => request.url.includes(api)))
       ) {
       this.key = makeStateKey<HttpResponse<object>>(request.url);
