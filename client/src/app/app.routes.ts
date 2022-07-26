d1c969b78dfbac72e84463b18a981697dbc9625c
Miroslav Smrtic <miro218@gmail.com>
2022-07-26 10:58:07 +0200

    fix(): jwtToken as standalone component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { AuthGuardAdmin } from './services/auth-admin.guard';
 import { AuthGuard } from './services/auth.guard';
-import { JwtTokenComponent } from './modules/auth/jwtToken/jwtToken.component';
 import { languages } from './shared/constants';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -18,8 +17,8 @@ const langRoutes = languages.map(lang => {
 
 export const routesAll = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'jwtToken/:accessToken', component: JwtTokenComponent },
   { path: '404', component: NotFoundComponent },
   ...[].concat(...langRoutes),
+  { path: 'jwtToken/:accessToken', loadComponent: () => import('./modules/auth/jwtToken/jwtToken.component').then(mod => mod.JwtTokenComponent)},
   { path: '**', redirectTo: '404' }
 ];
--- client/src/app/modules/auth/jwtToken/jwtToken.component.ts
+++ client/src/app/modules/auth/jwtToken/jwtToken.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 import { map, take, filter } from 'rxjs/operators';
@@ -9,30 +9,31 @@ import * as fromRoot from '../../../store/reducers';
 import { accessTokenKey } from '../../../shared/constants';
 
 @Component({
+  standalone: true,
   templateUrl: './jwtToken.component.html',
-  styleUrls: ['./jwtToken.component.scss']
+  styleUrls: ['./jwtToken.component.scss'],
 })
-export class JwtTokenComponent  {
-
+export class JwtTokenComponent implements OnInit {
   constructor(
-      private route  : ActivatedRoute,
-      private router : Router,
-      private store: Store<fromRoot.State>,
-      @Inject(PLATFORM_ID)
-      private platformId : Object) {
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<fromRoot.State>,
+    @Inject(PLATFORM_ID)
+    private platformId: Object
+  ) {}
 
-    this.route.params.pipe(
-        map(params => params['accessToken']),
+  ngOnInit(): void {
+    this.route.params
+      .pipe(
+        map((params) => params['accessToken']),
         take(1),
-        filter(() => isPlatformBrowser(this.platformId)))
-      .subscribe(accessToken => {
+        filter(() => isPlatformBrowser(this.platformId))
+      )
+      .subscribe((accessToken) => {
         localStorage.setItem(accessTokenKey, accessToken);
-        this.store.dispatch(new actions.StoreUser({accessToken: accessToken}));
+        this.store.dispatch(new actions.StoreUser({ accessToken: accessToken }));
         this.store.dispatch(new actions.GetUser());
-        this.router.navigate(['/']);
-      })
+        this.router.navigate(['']);
+      });
   }
-
-
-
 }
