13a46d75c31a3a3c5812fa0657d62c386230a07c
Miroslav Smrtic <miro218@gmail.com>
2023-11-14 15:27:16 +0100

    fix: product images

--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -1,4 +1,3 @@
-import { product } from './../../../client/src/app/store/reducers/product.reducer';
 import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
